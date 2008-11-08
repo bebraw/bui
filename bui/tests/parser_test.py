4ed1f0479179aaaac7bf2088f7b19010b8b56142
bebraw <bebraw@dff6c430-ad89-11dd-9763-1166fc48c086>
2008-11-08 14:53:40 +0000

    Added a couple of parser tests.

--- bui/tests/parser_test.py
+++ bui/tests/parser_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from bui.container import VerticalContainer
+from bui.container import HorizontalContainer, VerticalContainer
+from bui.element import EmptyElement
 from bui.parser import parse_structure, read_yaml
 
 from structure import *
@@ -26,9 +27,76 @@ def test_parse_valid_structure_with_child_container():
     
     assert isinstance(root_container, VerticalContainer)
     assert root_container.width == 200
+    assert root_container.visible == True
     assert len(root_container.children) == 1
     
     child_container = root_container.children[0]
     
     assert isinstance(child_container, VerticalContainer)
+    assert child_container.name == 'foobar'
     assert child_container.width == 300
+    assert child_container.visible == False
+
+def test_parse_valid_structure_with_child_container():
+    root_container = parse_structure(structure_horizontal_container_child, globals())
+    
+    assert isinstance(root_container, VerticalContainer)
+    assert root_container.width == 200
+    assert len(root_container.children) == 1
+    
+    child_container = root_container.children[0]
+    
+    assert isinstance(child_container, HorizontalContainer)
+    assert child_container.height == 40
+    assert child_container.width == 100
+
+def test_parse_valid_structure_with_child_container():
+    root_container = parse_structure(structure_with_multiple_containers, globals())
+    
+    assert isinstance(root_container, VerticalContainer)
+    assert root_container.width == 200
+    assert len(root_container.children) == 2
+    
+    child_container = root_container.children[0]
+    
+    assert isinstance(child_container, HorizontalContainer)
+    assert child_container.width == 100
+    
+    child_container = root_container.children[1]
+    
+    assert isinstance(child_container, VerticalContainer)
+    assert child_container.width == 150
+
+def test_parse_valid_structure_with_child_container():
+    root_container = parse_structure(structure_with_empty_elements, globals())
+    
+    assert isinstance(root_container, VerticalContainer)
+    assert root_container.width == 200
+    assert len(root_container.children) == 2
+    
+    child_element = root_container.children[0]
+    
+    assert isinstance(child_element, EmptyElement)
+    assert child_element.width == 100
+    
+    child_element = root_container.children[1]
+    
+    assert isinstance(child_element, EmptyElement)
+    assert child_element.width == 50
+
+def test_parse_structure_with_uistructure():
+    root_container = parse_structure(structure_with_uistructure, globals())
+    
+    assert isinstance(root_container, VerticalContainer)
+    assert root_container.width == 300
+    assert len(root_container.children) == 2
+    
+    child_container = root_container.children[0]
+    
+    assert isinstance(child_container, VerticalContainer)
+    assert child_container.width == 400
+    
+    child_element = root_container.children[1]
+    
+    assert isinstance(child_element, EmptyElement)
+    assert child_element.width == 80
--- bui/tests/structure.py
+++ bui/tests/structure.py
@@ -4,6 +4,16 @@ structure_with_one_item = '''
 - some_item:
 '''
 
+structure_with_uistructure = '''
+VerticalContainer:
+    width: 300
+    children:
+        - UIStructure:
+            name: minimal_structure
+        - EmptyElement:
+            width: 80
+'''
+
 minimal_structure = '''
 VerticalContainer:
     width: 400
@@ -14,5 +24,36 @@ VerticalContainer:
     width: 200
     children:
         - VerticalContainer:
+            name: foobar
+            visible: False
             width: 300
+'''
+
+structure_horizontal_container_child = '''
+VerticalContainer:
+    width: 200
+    children:
+        - HorizontalContainer:
+            height: 40
+            width: 100
+'''
+
+structure_with_multiple_containers = '''
+VerticalContainer:
+    width: 200
+    children:
+        - HorizontalContainer:
+            width: 100
+        - VerticalContainer:
+            width: 150
+'''
+
+structure_with_empty_elements = '''
+VerticalContainer:
+    width: 200
+    children:
+        - EmptyElement:
+            width: 100
+        - EmptyElement:
+            width: 50
 '''
