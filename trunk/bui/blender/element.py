bb31f94ed41fc6364a11965f235cc9921ac07eb0
bebraw <bebraw@dff6c430-ad89-11dd-9763-1166fc48c086>
2008-11-10 14:01:23 +0000

    Added Slider element for Blender and started to sketch out a new example file that shows sliders based on currently available bones names as "SLIsomething" in pose mode. I need to rethink the mechanism that attaches a new user interface structure to current one to make this example work.

--- trunk/bui/blender/element.py
+++ trunk/bui/blender/element.py
@@ -8,14 +8,16 @@ except ImportError:
 from bui.element import AbstractElement
 
 class AbstractBlenderElement(AbstractElement):
-    suitable_values = ('name', 'width', 'height', 'tooltip', 'value', 'max_input_length', 'event_handler', 'variable', ) # these should be defined for each elem separately?
+    suitable_values = ('name', 'width', 'height', 'tooltip', 'value', 'max_input_length', 'event_handler', 'variable', 'min', 'max') # these should be defined for each elem separately? at least the parts that differ anyway!
     
     def __init__(self, args=None):
         self.name = ''
         self.event = 0
         self.tooltip = ''
-        self.value = 0
+        self.value = 0.0
         self.max_input_length = 0
+        self.min = 0.0
+        self.max = 1.0
         #self.width = 0
         #self.height = 0
         super(AbstractBlenderElement, self).__init__(args)
@@ -46,3 +48,8 @@ class Menu(AbstractBlenderElement):
     def render(self, coord):
         self.menu = Draw.Menu(self.name, self.event, coord.x, coord.y, self.width, self.height,
                          self.value, self.tooltip, self.update_value)
+
+class Slider(AbstractBlenderElement):
+    def render(self, coord):
+        self.slider = Draw.Slider(self.name, self.event, coord.x, coord.y, self.width, self.height,
+                                  self.value, self.min, self.max, False, self.tooltip, self.update_value)
