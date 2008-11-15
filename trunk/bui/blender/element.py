c206b1866012c46413512d8ae789300831f90386
bebraw <bebraw@dff6c430-ad89-11dd-9763-1166fc48c086>
2008-11-15 20:34:27 +0000

    Added Number (regular float) and IntNumber elements for Blender.
    Thanks to jesterKing for contribution. :)

--- trunk/bui/blender/element.py
+++ trunk/bui/blender/element.py
@@ -8,7 +8,7 @@ except ImportError:
 from bui.element import AbstractElement
 
 class AbstractBlenderElement(AbstractElement):
-    suitable_values = ('name', 'width', 'height', 'tooltip', 'value', 'max_input_length', 'event_handler', 'variable', 'min', 'max') # these should be defined for each elem separately? at least the parts that differ anyway!
+    suitable_values = ('name', 'width', 'height', 'tooltip', 'value', 'max_input_length', 'event_handler', 'variable', 'min', 'max', 'range', 'precision') # TODO: refactor
     
     def __init__(self, args=None):
         self.name = ''
@@ -20,6 +20,8 @@ class AbstractBlenderElement(AbstractElement):
         self.max = 1.0
         #self.width = 0
         #self.height = 0
+        self.range = 0 # no clickstep
+        self.precision = 0.0 # 4 decimals
         super(AbstractBlenderElement, self).__init__(args)
     
     def update_value(self, evt, val):
@@ -53,3 +55,17 @@ class Slider(AbstractBlenderElement):
     def render(self, coord):
         self.slider = Draw.Slider(self.name, self.event, coord.x, coord.y, self.width, self.height,
                                   self.value, self.min, self.max, False, self.tooltip, self.update_value)
+
+class Number(AbstractBlenderElement):
+    def render(self, coord):
+        try:
+            self.number = Draw.Number(self.name, self.event, coord.x, coord.y, self.width, self.height,
+                                  self.value, self.min, self.max, self.tooltip, self.update_value, self.range, self.precision)
+        except: # needed for backwards compatibility (no range and precision in 2.48a)
+            self.number = Draw.Number(self.name, self.event, coord.x, coord.y, self.width, self.height,
+                                  self.value, self.min, self.max, self.tooltip, self.update_value)
+
+class IntNumber(AbstractBlenderElement):
+    def render(self, coord):
+        self.number = Draw.Number(self.name, self.event, coord.x, coord.y, self.width, self.height,
+                                  int(self.value), int(self.min), int(self.max), self.tooltip, self.update_value)
