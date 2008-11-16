b52091d171fe8663f7afe9e7fce863907c53481e
bebraw <bebraw@dff6c430-ad89-11dd-9763-1166fc48c086>
2008-11-16 08:05:54 +0000

    Got rid of redundant code.

--- bui/blender/event.py
+++ bui/blender/event.py
@@ -19,12 +19,3 @@ class BlenderEventManager(EventManager):
     
     def _construct_key_event_ids(self, keys, key_mapping=None):
         super(BlenderEventManager, self)._construct_key_event_ids(keys, BLENDER_KEYS)
-        '''
-        keys_structure = read_yaml(keys)
-        
-        if isinstance(keys_structure, dict):
-            for key, func_name in keys_structure.items():
-                if self.namespace.has_key(func_name):
-                    blender_key = BLENDER_KEYS[key]
-                    self.key_events[blender_key] = self.namespace[func_name]
-        '''
